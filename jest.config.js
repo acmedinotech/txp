0af7bb9dc4c195052b13a62af5f07ea524e23b2d
kaiser shahid <kaisershahid@gmail.com>
2023-04-30 15:47:53 -0400

    - tweaks

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
+module.exports = {
 	roots: ['<rootDir>/src'],
 	setupFilesAfterEnv: ['./jest.d.ts', './jest.extend.js'],
 	preset: 'ts-jest',
